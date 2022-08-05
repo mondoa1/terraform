969fe6d0e64feb2ae70dbf38e5c565444484f6f8
mondoa1 <99384006+mondoa1@users.noreply.github.com>
2022-08-04 20:56:59 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@
    backend "s3" {
     bucket = "janviers-tfbuck"
     key    = "avinash/s3file-3.tfstate"
-    region = "us-east-1"
+    region = "us-east-1a"
     profile = "jespo"
 dynamodb_table= "firstprojecttable"
    }
